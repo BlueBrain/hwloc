3a075aede644bf8c5f2ae3678c8b7fbce9a05544
sthibaul <sthibaul@4b44e086-7f34-40ce-a3bd-00e031736276>
2011-12-14 17:22:11 +0000

    stricter C syntax
    git-svn-id: http://svn.open-mpi.org/svn/hwloc/branches/cuda@4057 4b44e086-7f34-40ce-a3bd-00e031736276

--- src/topology.c
+++ src/topology.c
@@ -395,7 +395,7 @@ static unsigned obj_type_order[] = {
     /* next entry is HWLOC_OBJ_MISC */     11,
     /* next entry is HWLOC_OBJ_BRIDGE */   7,
     /* next entry is HWLOC_OBJ_PCI_DEVICE */  8,
-    /* next entry is HWLOC_OBJ_OS_DEVICE */   9,
+    /* next entry is HWLOC_OBJ_OS_DEVICE */   9
 };
 
 static const hwloc_obj_type_t obj_order_type[] = {
