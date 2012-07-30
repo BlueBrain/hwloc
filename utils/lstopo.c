be6124e6afa87367dcdab0dc50d29f7acd7d8bb2
arougier <arougier@4b44e086-7f34-40ce-a3bd-00e031736276>
2012-07-30 08:10:58 +0000

    Remove a few of FIXMEs
    git-svn-id: http://svn.open-mpi.org/svn/hwloc/branches/backends@4668 4b44e086-7f34-40ce-a3bd-00e031736276

--- utils/lstopo.c
+++ utils/lstopo.c
@@ -125,7 +125,7 @@ static void add_process_objects(hwloc_topology_t topology)
 
     proc_cpubind = hwloc_get_proc_cpubind(topology, local_pid, cpuset, 0) != -1;
 
-/* FIXME Discuter afin de trouver une solution #ifdef HWLOC_LINUX_SYS */
+/* FIXME #ifdef HWLOC_LINUX_SYS */
 #if 0
     {
       /* Get the process name */
