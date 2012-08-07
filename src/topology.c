BlueBrain/hwloc

commit f66622ff34c9ad71859cf8e51b7aeac2da9dd04d

Rework the core libpci import so that we can honor I/O filtering flags when importing from XML
We'll backport into v1.3 if somebody complains before v1.4 is released

git-svn-id: http://svn.open-mpi.org/svn/hwloc/trunk@3932 4b44e086-7f34-40ce-a3bd-00e031736276

src/topology.c
@@ -1987,6 +1987,8 @@ static void hwloc_topology_setup_defaults(struct hwloc_topology *topology);
 static int
 hwloc_discover(struct hwloc_topology *topology)
 {
+  int gotsomeio = 1;
+
   if (topology->backend_type == HWLOC_BACKEND_SYNTHETIC) {
     alloc_cpusets(topology->levels[0][0]);
     hwloc_look_synthetic(topology);
@@ -2186,40 +2188,36 @@ hwloc_discover(struct hwloc_topology *topology)
    * and there.
    */
 
-  /* PCI */
-  if (topology->flags & (HWLOC_TOPOLOGY_FLAG_IO_DEVICES|HWLOC_TOPOLOGY_FLAG_WHOLE_IO)) {
-    int gotsome = 0;
+  /* I/O devices */
+
+  /* see if the backend already imported some I/O devices */
+  if (topology->backend_type == HWLOC_BACKEND_XML
+      || topology->backend_type == HWLOC_BACKEND_SYNTHETIC)
+    gotsomeio = 1;
+  /* import from libpci if needed */
+  if (topology->flags & (HWLOC_TOPOLOGY_FLAG_IO_DEVICES|HWLOC_TOPOLOGY_FLAG_WHOLE_IO)
+      && (topology->backend_type == HWLOC_BACKEND_NONE
+	  || topology->backend_type == HWLOC_BACKEND_LINUXFS)) {
     hwloc_debug("%s", "\nLooking for PCI devices\n");
-
-    if (topology->backend_type == HWLOC_BACKEND_SYNTHETIC) {
-      /* TODO */
-    }
-    else if (topology->backend_type == HWLOC_BACKEND_XML) {
-      /* TODO */
-    }
 #ifdef HWLOC_HAVE_LIBPCI
-    else if (topology->is_thissystem) {
+    if (topology->is_thissystem) {
       hwloc_look_libpci(topology);
-      gotsome = 1;
-    }
+      print_objects(topology, 0, topology->levels[0][0]);
+      gotsomeio = 1;
+    } else
 #endif
-
-    if (gotsome) {
-      print_objects(topology, 0, topology->levels[0][0]);
-
-      hwloc_drop_useless_io(topology, topology->levels[0][0]);
-
-      hwloc_propagate_bridge_depth(topology, topology->levels[0][0], 0);
-
-      hwloc_debug("%s", "\nNow reconnecting\n");
-
-      hwloc_connect_children(topology->levels[0][0]);
-      hwloc_connect_levels(topology);
-
-      print_objects(topology, 0, topology->levels[0][0]);
-    } else {
+    {
       hwloc_debug("%s", "\nno PCI detection\n");
     }
+  }
+  /* if we got anything, filter interesting objects and update the tree */
+  if (gotsomeio) {
+    hwloc_drop_useless_io(topology, topology->levels[0][0]);
+    hwloc_debug("%s", "\nNow reconnecting\n");
+    hwloc_connect_children(topology->levels[0][0]);
+    hwloc_connect_levels(topology);
+    print_objects(topology, 0, topology->levels[0][0]);
+    hwloc_propagate_bridge_depth(topology, topology->levels[0][0], 0);
   }
 
   /*

commit d0282d3232d8c837faa4f4ea4a7f1e3c7ef28d1f

Don't always reconnect levels as if we always got some new PCI devices
Fix a typo in initialization, introduced in r3932

git-svn-id: http://svn.open-mpi.org/svn/hwloc/trunk@4689 4b44e086-7f34-40ce-a3bd-00e031736276

src/topology.c
@@ -2137,7 +2137,7 @@ static void hwloc_topology_setup_defaults(struct hwloc_topology *topology);
 static int
 hwloc_discover(struct hwloc_topology *topology)
 {
-  int gotsomeio = 1;
+  int gotsomeio = 0;
 
   if (topology->backend_type == HWLOC_BACKEND_SYNTHETIC) {
     alloc_cpusets(topology->levels[0][0]);