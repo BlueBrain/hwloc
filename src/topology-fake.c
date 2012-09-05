9304758a6cd6a0d4dccca1e11f534df582f7e66c
bgoglin <bgoglin@4b44e086-7f34-40ce-a3bd-00e031736276>
2012-09-05 08:13:23 +0000

    Add a topology-fake.c built as a plugin in make check
    The plugin is registered as an additional core plugin
    (so that it always gets enabled by default).
    
    Its instantiate callback() does nothing but printing something
    if HWLOC_DEBUG_FAKE_COMPONENT is set in the environment.
    We could grep for this output in a make check test.
    
    It needs "-rpath /nowhere" in LDFLAGS because shared libraries
    are only built entirely when placed in lib_LTLIBRARIES
    (this fake one is in check_LTLIBRARIES only).
    
    git-svn-id: http://svn.open-mpi.org/svn/hwloc/branches/components@4812 4b44e086-7f34-40ce-a3bd-00e031736276

---
+++ src/topology-fake.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright © 2012 Inria.  All rights reserved.
+ * See COPYING in top-level directory.
+ */
+
+#include <private/autogen/config.h>
+#include <hwloc.h>
+#include <private/private.h>
+
+#include <stdlib.h>
+
+static int
+hwloc_fake_component_instantiate(struct hwloc_topology *topology __hwloc_attribute_unused,
+				 struct hwloc_core_component *component __hwloc_attribute_unused,
+				 const void *_data1 __hwloc_attribute_unused,
+				 const void *_data2 __hwloc_attribute_unused,
+				 const void *_data3 __hwloc_attribute_unused)
+{
+  if (getenv("HWLOC_DEBUG_FAKE_COMPONENT"))
+    printf("fake component instantiated\n");
+  return 0;
+}
+
+static struct hwloc_core_component hwloc_fake_core_component = {
+  HWLOC_CORE_COMPONENT_TYPE_ADDITIONAL, /* so that it's always enabled when using the OS discovery */
+  "fake",
+  hwloc_fake_component_instantiate,
+  NULL,
+  10,
+  NULL
+};
+
+HWLOC_DECLSPEC struct hwloc_component hwloc_core_fake_component = {
+  HWLOC_COMPONENT_ABI,
+  HWLOC_COMPONENT_TYPE_CORE,
+  &hwloc_fake_core_component
+};
