0dde24ee3f86fc60c3f2596baf18bb014e8042cc
bgoglin <bgoglin@4b44e086-7f34-40ce-a3bd-00e031736276>
2012-11-05 10:32:47 +0000

    darwin: fix commit r4997 * components: remove the topology from the instantiate callback
    git-svn-id: http://svn.open-mpi.org/svn/hwloc/branches/components@5005 4b44e086-7f34-40ce-a3bd-00e031736276

--- src/topology-darwin.c
+++ src/topology-darwin.c
@@ -277,8 +277,7 @@ hwloc_set_darwin_hooks(struct hwloc_binding_hooks *hooks __hwloc_attribute_unuse
 }
 
 static struct hwloc_backend *
-hwloc_darwin_component_instantiate(struct hwloc_topology *topology,
-				   struct hwloc_disc_component *component,
+hwloc_darwin_component_instantiate(struct hwloc_disc_component *component,
 				   const void *_data1 __hwloc_attribute_unused,
 				   const void *_data2 __hwloc_attribute_unused,
 				   const void *_data3 __hwloc_attribute_unused)
